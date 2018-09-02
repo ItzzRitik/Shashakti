a32e0d490b70303d6a5cb67e93815b4c63d89ee1
Ritik Srivastava <ritik.space@gmail.com>
2018-09-02 06:14:08 +0530

    Display Schemes

--- app/src/main/java/io/skyhack/skyhack/SchemeActivity.java
+++ app/src/main/java/io/skyhack/skyhack/SchemeActivity.java
@@ -1,13 +1,67 @@
 package io.skyhack.skyhack;
 
+import android.animation.ValueAnimator;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Interpolator;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class SchemeActivity extends AppCompatActivity {
 
+    RelativeLayout splash_cover,logo_div;
+    TextView page_tag;
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scheme);
+
+        splash_cover=findViewById(R.id.splash_cover);
+        logo_div=findViewById(R.id.logo_div);
+        page_tag=findViewById(R.id.page_tag);
+        back.findViewById(R.id.back);
+
+        splash_cover.setVisibility(View.GONE);
+        logo_div.setVisibility(View.VISIBLE);
+        page_tag.setVisibility(View.VISIBLE);
+        back.setVisibility(View.VISIBLE);
+    }
+    public void scaleX(final View view,int x,int t, Interpolator interpolator)
+    {
+        ValueAnimator anim = ValueAnimator.ofInt(view.getMeasuredWidth(),(int)dptopx(x));anim.setInterpolator(interpolator);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+                layoutParams.width = (Integer) valueAnimator.getAnimatedValue();
+                view.setLayoutParams(layoutParams);
+            }
+        });
+        anim.setDuration(t);anim.start();
+    }
+    public void scaleY(final View view,int y,int t, Interpolator interpolator)
+    {
+        ValueAnimator anim = ValueAnimator.ofInt(view.getMeasuredHeight(),(int)dptopx(y));anim.setInterpolator(interpolator);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+                layoutParams.height = (Integer) valueAnimator.getAnimatedValue();
+                view.setLayoutParams(layoutParams);view.invalidate();
+            }
+        });
+        anim.setDuration(t);anim.start();
+    }
+    public float dptopx(float num)
+    {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, num, getResources().getDisplayMetrics());
     }
 }
